2fbae311411861c162abffef32b512ab56634296
obedm503 <obedm503@gmail.com>
2017-05-23 11:25:42 -0600

    added bootstrap

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+window.mandala = (()=>{
+  let calculator;
   function randomColor(){
     return '#' + Math.random().toString(16).slice(2, 8).toUpperCase();
   }
   function randomNumbers(range, amount){
     return Array(amount)
       .fill('')
-      .map( () => Math.floor( range * ( Math.random() * 2 - 1 ) ) );
+      .map( () => Math.random() * 2 - 1  )
+      .map( num => num > 0 ? num : 2 )
+      .map( num => Math.floor( range * num ) );
   }
   function lines(numbers){
     return numbers.map( num => `y=${num}(x+${num})` );
@@ -45,19 +48,22 @@ document.addEventListener("DOMContentLoaded", ()=>{
     }, []);
   }
 
-  window.mandala = {
-    screenshot(){
-      window.screenshot = calculator.screenshot();
-      let a = document.createElement('a');
-      a.href = window.screenshot;
-      a.download = 'random-mandala';
-      document.body.appendChild(a);
-      a.click()
-      document.body.removeChild(a);
-    },
+  document.addEventListener("DOMContentLoaded", ()=>{
+     calculator = Desmos.GraphingCalculator(
+      document.getElementById('calculator'), {
+      keypad: false,
+      expressionsCollapsed: true
+    });
+    // calculator.setState({"version":3,"graph":{"showGrid":true,"showXAxis":false,"showYAxis":false,"xAxisStep":0,"yAxisStep":0,"xAxisMinorSubdivisions":0,"yAxisMinorSubdivisions":0,"xAxisArrowMode":"NONE","yAxisArrowMode":"NONE","xAxisLabel":"","yAxisLabel":"","xAxisNumbers":false,"yAxisNumbers":false,"polarMode":true,"polarNumbers":false,"degreeMode":true,"projectorMode":false,"squareAxes":true,"viewport":{"xmin":-10,"ymin":-8.527027027027028,"xmax":10,"ymax":8.527027027027028}},"expressions":{"list":[{"id":"47","type":"expression","latex":"t=-0.0333","domain":{"min":"0","max":"1"},"label":"","hidden":true,"secret":false,"color":"#6042a6","style":"normal","dragMode":"AUTO","residualVariable":"","regressionParameters":{},"isLogModeRegression":false,"sliderMin":"-.5","sliderMax":"0.5","sliderHardMin":true,"sliderHardMax":true,"sliderInterval":"","sliderAnimationPeriod":8000,"sliderLoopMode":"LOOP_FORWARD_REVERSE","sliderPlayDirection":1,"sliderIsPlaying":false},{"id":"20","type":"expression","latex":"y=t^{\\pi^2}x^{-2}","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#2d70b3","style":"normal","dragMode":"AUTO","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"22","type":"expression","latex":"y=-t^{\\pi^2}x^{-2}","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#6042a6","style":"normal","dragMode":"AUTO","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"27","type":"expression","latex":"x=t^{\\pi^2}y^{-2}","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#388c46","style":"normal","dragMode":"AUTO","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"28","type":"expression","latex":"x=-t^{\\pi^2}y^{-2}","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#fa7e19","style":"normal","dragMode":"AUTO","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"19","type":"expression","latex":"r=t\\frac{\\theta}{\\pi}\\cos5\\theta","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#fa7e19","style":"normal","dragMode":"AUTO","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"30","type":"expression","latex":"r=-t\\frac{\\theta}{\\pi}\\cos5\\theta","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#6042a6","style":"normal","dragMode":"NONE","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"32","type":"expression","latex":"r=20t\\frac{\\theta}{\\sqrt{\\left|t\\right|}}","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#000000","style":"dashed","dragMode":"NONE","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"33","type":"expression","latex":"r=-20t\\frac{\\theta}{\\sqrt{\\left|t\\right|}}","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#c74440","style":"dashed","dragMode":"NONE","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"34","type":"expression","latex":"r=-\\theta t\\cos\\theta","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#2d70b3","style":"dashed","dragMode":"NONE","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"39","type":"expression","latex":"r=\\theta t\\cos\\theta","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#388c46","style":"dotted","dragMode":"NONE","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"42","type":"expression","latex":"r=t\\theta","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#6042a6","style":"normal","dragMode":"NONE","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"43","type":"expression","latex":"r=-t\\theta","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#c74440","style":"normal","dragMode":"NONE","residualVariable":"","regressionParameters":{},"isLogModeRegression":false}]}})
+  });
+
+  return {
     graph(){
+      let range = parseFloat(document.getElementById('range').value);
+      let amount = parseInt(document.getElementById('amount').value);
+
       calculator.setBlank();
-      let numbers = randomNumbers(20,1);
+      let numbers = randomNumbers(range + 1, amount);
       let expressions = [
         ...lines(numbers),
         ...curves(numbers),
@@ -66,14 +72,19 @@ document.addEventListener("DOMContentLoaded", ()=>{
         ...flowers(numbers)
       ]
       expressions.filter( ( ex, i, arr ) => arr.indexOf(ex) === i )
-        .forEach( ex => {console.log(ex);return calculator.setExpression({ latex: ex, color: randomColor() }) });
+        .forEach( exp => calculator.setExpression({
+          latex: exp,
+          color: randomColor()
+        }) );
+    },
+    screenshot(){
+      window.screenshot = calculator.screenshot();
+      let a = document.createElement('a');
+      a.href = window.screenshot;
+      a.download = 'random-mandala';
+      document.body.appendChild(a);
+      a.click()
+      document.body.removeChild(a);
     }
-  };
-
- var calculator = window.calculator = Desmos.GraphingCalculator(document.getElementById('calculator'), {
-    keypad: false,
-    expressionsCollapsed: true
-  });
-  // calculator.setState({"version":3,"graph":{"showGrid":true,"showXAxis":false,"showYAxis":false,"xAxisStep":0,"yAxisStep":0,"xAxisMinorSubdivisions":0,"yAxisMinorSubdivisions":0,"xAxisArrowMode":"NONE","yAxisArrowMode":"NONE","xAxisLabel":"","yAxisLabel":"","xAxisNumbers":false,"yAxisNumbers":false,"polarMode":true,"polarNumbers":false,"degreeMode":true,"projectorMode":false,"squareAxes":true,"viewport":{"xmin":-10,"ymin":-8.527027027027028,"xmax":10,"ymax":8.527027027027028}},"expressions":{"list":[{"id":"47","type":"expression","latex":"t=-0.0333","domain":{"min":"0","max":"1"},"label":"","hidden":true,"secret":false,"color":"#6042a6","style":"normal","dragMode":"AUTO","residualVariable":"","regressionParameters":{},"isLogModeRegression":false,"sliderMin":"-.5","sliderMax":"0.5","sliderHardMin":true,"sliderHardMax":true,"sliderInterval":"","sliderAnimationPeriod":8000,"sliderLoopMode":"LOOP_FORWARD_REVERSE","sliderPlayDirection":1,"sliderIsPlaying":false},{"id":"20","type":"expression","latex":"y=t^{\\pi^2}x^{-2}","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#2d70b3","style":"normal","dragMode":"AUTO","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"22","type":"expression","latex":"y=-t^{\\pi^2}x^{-2}","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#6042a6","style":"normal","dragMode":"AUTO","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"27","type":"expression","latex":"x=t^{\\pi^2}y^{-2}","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#388c46","style":"normal","dragMode":"AUTO","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"28","type":"expression","latex":"x=-t^{\\pi^2}y^{-2}","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#fa7e19","style":"normal","dragMode":"AUTO","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"19","type":"expression","latex":"r=t\\frac{\\theta}{\\pi}\\cos5\\theta","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#fa7e19","style":"normal","dragMode":"AUTO","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"30","type":"expression","latex":"r=-t\\frac{\\theta}{\\pi}\\cos5\\theta","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#6042a6","style":"normal","dragMode":"NONE","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"32","type":"expression","latex":"r=20t\\frac{\\theta}{\\sqrt{\\left|t\\right|}}","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#000000","style":"dashed","dragMode":"NONE","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"33","type":"expression","latex":"r=-20t\\frac{\\theta}{\\sqrt{\\left|t\\right|}}","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#c74440","style":"dashed","dragMode":"NONE","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"34","type":"expression","latex":"r=-\\theta t\\cos\\theta","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#2d70b3","style":"dashed","dragMode":"NONE","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"39","type":"expression","latex":"r=\\theta t\\cos\\theta","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#388c46","style":"dotted","dragMode":"NONE","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"42","type":"expression","latex":"r=t\\theta","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#6042a6","style":"normal","dragMode":"NONE","residualVariable":"","regressionParameters":{},"isLogModeRegression":false},{"id":"43","type":"expression","latex":"r=-t\\theta","domain":{"min":"0","max":"1"},"label":"","hidden":false,"secret":false,"color":"#c74440","style":"normal","dragMode":"NONE","residualVariable":"","regressionParameters":{},"isLogModeRegression":false}]}})
-
-});
+  }
+})();
