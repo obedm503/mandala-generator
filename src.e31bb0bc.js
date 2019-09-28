e034e3e7320b15825f32b3bc0536f2ee7bc0d0aa
obedm503 <obedm503@gmail.com>
2019-09-28 00:03:52 -0500

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -292,7 +292,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37735" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40485" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -468,4 +468,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/src.e31bb0bc.js.map
+//# sourceMappingURL=mandala-generator/src.e31bb0bc.js.map
